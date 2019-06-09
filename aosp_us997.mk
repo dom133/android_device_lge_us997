9b4c265adc94c985e4838da881340bce4866ef5c
dom133 <dom133_pl@protonmail.com>
2019-06-09 21:24:42 +0200

    us997: update fingerprint

--- aosp_us997.mk
+++ aosp_us997.mk
@@ -38,6 +38,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lucye" \
     PRODUCT_DEVICE="lucye" \
     PRODUCT_NAME="lucye_nao_us" \
-    PRIVATE_BUILD_DESC="lucye_nao_us-user 8.0.0 OPR1.170623.032 1832614181866 release-keys"
+    PRIVATE_BUILD_DESC="lucye_nao_us-user 8.0.0 OPR1.170623.032 190420940e75c release-keys"
 
-BUILD_FINGERPRINT := "lge/lucye_nao_us/lucye:8.0.0/OPR1.170623.032/1832614181866:user/release-keys"
+BUILD_FINGERPRINT := "lge/lucye_nao_us/lucye:8.0.0/OPR1.170623.032/190420940e75c:user/release-keys"
